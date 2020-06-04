cce0f240888af4b801dc3727bdc8162e0f91075a
liuwei <liuwei@shijidean.com>
2020-06-04 18:08:17 +0800

    *1

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ npm run build # 生成静态文件
 cd docs/.vuepress/dist # 进入生成的文件夹
 
 # deploy to github
-echo 'zerodegree.top' > CNAME
-if [ -z "$ACCESS_TOKEN" ]; then
+echo 'blog.zerodegree.top' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:zerocola777/my-blog.git
 else
   msg='来自github action的自动部署'
-  githubUrl=https://zerocola777:${ACCESS_TOKEN}@github.com/zerocola777/my-blog.git
+  githubUrl=https://zerocola777:${GITHUB_TOKEN}@github.com/zerocola777/my-blog.git
 fi
 git init
 git add -A
