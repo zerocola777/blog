f7f2d5d29c581c7c25d71ed1612fb92c131d113a
liuwei <liuwei@shijidean.com>
2020-06-04 17:10:31 +0800

    *1

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
-# 生成静态文件
-npm run docs:build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# deploy to github
+echo 'zerodegree.top' > CNAME
+if [ -z "$ACCESS_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:zerocola777/my-blog.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://zerocola777:${ACCESS_TOKEN}@github.com/zerocola777/my-blog.git
+  git config --global user.name "zerodegree"
+  git config --global user.email "77849093@qq.com"
+fi
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
 
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:zerocola777/zerocola777.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# deploy to coding
+echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  codingUrl=git@e.coding.net:zerodegree/my-blog.git
+else
+  codingUrl=https://xugaoyi:${CODING_TOKEN}@git@e.coding.net:zerodegree/my-blog.git
+fi
+git add -A
+git commit -m "${msg}"
+git push -f $codingUrl master # 推送到coding
 
 cd -
+rm -rf docs/.vuepress/dist
