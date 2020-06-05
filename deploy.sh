0058d5a8d46a9204accf2191e7813324b2c6cc5a
liuwei <liuwei@shijidean.com>
2020-06-05 10:41:46 +0800

    *2

--- deploy.sh
+++ deploy.sh
@@ -23,9 +23,9 @@ git push -f $githubUrl master:gh-pages # 推送到github
 # deploy to coding
 echo 'www.zerodegree.top\nzerodegree.top' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:zerodegree/blog.git
+  codingUrl=git@e.coding.net:zerodegree/zerodegree.git
 else
-  codingUrl=https://zerodegree:${CODING_TOKEN}@e.coding.net/zerodegree/blog.git
+  codingUrl=https://zerodegree:${CODING_TOKEN}@e.coding.net/zerodegree/zerodegree.git
 fi
 git add -A
 git commit -m "${msg}"
